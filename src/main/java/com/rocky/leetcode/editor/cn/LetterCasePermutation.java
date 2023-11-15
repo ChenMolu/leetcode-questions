fc9dd28280f58b6d4904f0cef81753d9a9c1e0c2
Rocky <993916778@qq.com>
2023-11-15 23:59:25 +0800

    784字母大小写全排列

---
+++ src/main/java/com/rocky/leetcode/editor/cn/LetterCasePermutation.java
@@ -0,0 +1,68 @@
+//给定一个字符串 s ，通过将字符串 s 中的每个字母转变大小写，我们可以获得一个新的字符串。
+//
+// 返回 所有可能得到的字符串集合 。以 任意顺序 返回输出。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "a1b2"
+//输出：["a1b2", "a1B2", "A1b2", "A1B2"]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "3z4"
+//输出: ["3z4","3Z4"]
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length <= 12 
+// s 由小写英文字母、大写英文字母和数字组成 
+// 
+//
+// Related Topics 位运算 字符串 回溯 👍 561 👎 0
+
+
+package com.rocky.leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCasePermutation {
+    public static void main(String[] args) {
+        Solution solution = new LetterCasePermutation().new Solution();
+        solution.letterCasePermutation("a1b2");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        List<String> result = new ArrayList<>();
+        public List<String> letterCasePermutation(String s) {
+            backTracing(s.toCharArray(), 0);
+            return result;
+        }
+
+        public void backTracing(char[] s, int startIndex) {
+            while(startIndex < s.length && Character.isDigit(s[startIndex])){
+                startIndex++;
+            }
+            if (startIndex == s.length) {
+                result.add(new String(s));
+                return;
+            }
+            s[startIndex] ^= 32;
+            backTracing(s, startIndex + 1);
+            s[startIndex] ^= 32;
+            backTracing(s, startIndex+1);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
