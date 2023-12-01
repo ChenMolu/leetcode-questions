22dc910cabd54990ef2709e10e33c9a43cfc3964
Rocky <993916778@qq.com>
2023-12-01 23:28:35 +0800

    199.二叉树的右视图

---
+++ src/main/java/com/rocky/leetcode/editor/cn/BinaryTreeRightSideView.java
@@ -0,0 +1,92 @@
+//给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入: [1,2,3,null,5,null,4]
+//输出: [1,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [1,null,3]
+//输出: [1,3]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: []
+//输出: []
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 二叉树的节点个数的范围是 [0,100] 
+// 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 997 👎 0
+
+
+package com.rocky.leetcode.editor.cn;
+
+import com.rocky.leetcode.editor.cn.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeRightSideView {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeRightSideView().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<Integer> rightSideView(TreeNode root) {
+            List<Integer> path = new ArrayList<>();
+            boolean[] vis = new boolean[105];
+            int dep = 0;
+            dfs(root, path, vis, dep);
+            return path;
+        }
+
+        public void dfs(TreeNode root, List<Integer> path, boolean[] vis, int dep) {
+            if (root == null) {
+                return;
+            }
+            if (!vis[dep]) {
+                path.add(root.val);
+                vis[dep] = true;
+            }
+            dfs(root.right, path, vis, dep + 1);
+            dfs(root.left, path, vis, dep + 1);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
